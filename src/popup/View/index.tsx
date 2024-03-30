221b847aaf80fe04b19b954b0c1015a434ed6d91
slava <slava22305@mail.ru>
2024-03-30 22:10:48 +0300

    Init commit

---
+++ src/popup/View/index.tsx
@@ -0,0 +1,15 @@
+
+
+export const View = () => {
+    return (
+        <div style={{
+            height: '100vh',
+            fontSize: '24px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            test
+        </div>
+    )
+}
