0e3d1a66e0a23f523f6dcf7bc5405157e20b8f10
slava <slava22305@mail.ru>
2024-03-31 20:09:26 +0300

    all works for click plugin

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,3 +1,56 @@
+import api, { route } from "@forge/api";
+import { runtime } from "webextension-polyfill";
+
 console.log("[content] loaded")
 
-export {}
+type Listener = (event: MouseEvent) => void
+
+let count = 0
+
+function registerClickListener(listener: Listener) {
+    window.addEventListener('click', listener)
+}
+
+function countClicks() {
+    count++
+    console.log('click(): ', count)
+
+    return runtime.sendMessage({
+        from: 'content',
+        to: 'background',
+        action: 'click'
+    })
+}
+
+export function init() {
+    registerClickListener(countClicks)
+}
+
+// const response = await api.asUser().requestJira(route`/rest/api/2/task/{taskId}`, {
+//   headers: {
+//     'Accept': 'application/json'
+//   }
+// });
+
+// async function resp() {
+//     const response = await api.asUser().requestJira(route`/rest/api/2/task/{taskId}`, {
+//         headers: {
+//           'Accept': 'application/json'
+//         }
+//       });
+//     return response
+// }
+// console.log(resp())
+
+// const response2 = async () => {
+//     await api.asUser().requestJira(route`/rest/api/2/task/{taskId}`, {
+//     headers: {
+//       'Accept': 'application/json'
+//     }
+//   });
+// }
+
+//console.log(`Response: ${response2.arguments.status} ${response2.arguments.statusText}`);
+//console.log(await response.json());
+
+init()
--- src/popup/View/index.tsx
+++ src/popup/View/index.tsx
@@ -1,6 +1,54 @@
-
+import { useEffect, useState } from "react"
+import { Version3Client } from 'jira.js';
+import { getCurrentTab } from "../../helpers/tabs";
+import { storage } from "webextension-polyfill";
 
 export const View = () => {
+
+    const [value, setValue] = useState()
+
+    useEffect(() => {
+        const readBackgroundMessage = async () => {
+            const tab = await getCurrentTab()
+            const tabId = tab.id
+
+            if (tabId) {
+                const data = await storage.local.get(tabId.toString())
+                const currentValue = data?.[tabId] ?? 0
+
+                setValue(currentValue)
+            }
+        }
+        readBackgroundMessage()
+    }, [])
+
+    // const [value, setValue] = useState<any | null>(null);
+
+    // useEffect(() => {
+
+    //     const readInfo = async () => {
+    //         const client = new Version3Client({
+    //             host: 'http://localhost:2990/jira',
+    //             authentication: {
+    //               basic: {
+    //                 username: 'admin',
+    //                 password: 'admin',
+    //               },
+    //             },
+    //           });
+    
+    //         //const projects = await client.projects.getProject();
+    //         const issues = await client.issues.getEvents(); 
+    //         const myself = await client.myself.getCurrentUser();
+    //         //const currentValue = issues?.toString
+    //         const currentValue = [issues, myself]
+    //         console.log(currentValue)
+    //         setValue(currentValue)
+    //     }
+    //     readInfo()
+    // }, [])
+
+    //const value = 0
     return (
         <div style={{
             height: '100vh',
@@ -9,7 +57,7 @@ export const View = () => {
             alignItems: 'center',
             justifyContent: 'center',
         }}>
-            test
+            Clicks: {value}
         </div>
     )
 }
